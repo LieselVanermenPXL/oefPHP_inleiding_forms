364a3024fa111542eb3afb66882e8c3cd03b232c
LieselVanermenPXL <44004455+LieselVanermenPXL@users.noreply.github.com>
2020-04-11 18:46:28 +0200

    Oefening 3

--- oefening3/src/php/statistiek.php
+++ oefening3/src/php/statistiek.php
@@ -1,2 +1,19 @@
 <?php
-var_dump($_GET['getallen']);
+$getallen = $_GET['getallen'];
+$wrongValues = array();
+
+foreach ($getallen as $getal) {
+    if (!is_numeric($getal)) {
+        array_push($wrongValues, $getal);
+        array_splice($getallen, array_search($getal, $getallen), 1);
+    }
+}
+
+print("Som = " . array_sum($getallen) . "</br>");
+print("Min = " . min($getallen) . "</br>");
+print("Max = " . max($getallen) . "</br>");
+
+print("</br>Wrong values: </br>");
+foreach ($wrongValues as $value) {
+    print(" - " . $value . "</br>");
+}
