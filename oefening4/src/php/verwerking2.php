a45448a63a36f322bb03f28b610a4f78c4ffc08a
LieselVanermenPXL <44004455+LieselVanermenPXL@users.noreply.github.com>
2020-04-16 11:13:09 +0200

    Oefening4 - update comment

--- oefening4/src/php/verwerking2.php
+++ oefening4/src/php/verwerking2.php
@@ -7,4 +7,4 @@ setcookie('kleur', $kleur, time() + 5 * 60 * 60);
 
 require_once('toon.php');
 
-// werkt ook en werkt sowieso met cookie
+// werkt ook en werkt sowieso met cookie (variabalen namen waar cookie gemaakt wordt in toon.php nog aanpassen dan)
